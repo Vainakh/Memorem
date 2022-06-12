70e5d4b3da7f2fe287ac2f4a6d1bd9d9155ef59d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-06-12 16:25:50 -0500

    Set up routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
 import express from 'express';
-import bodyParse from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Vainakh:Memorem2022$@cluster0.lmu5xfh.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5001;
+
+mongoose
+    .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then(() =>
+    app.listen (PORT, () => console.log(`Server running on port: ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));
+
+
+
